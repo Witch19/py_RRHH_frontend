24e122600382dbed98dee7b5d68ee7e11580b116
JOSS COMPU <josytst.js@gmail.com>
2025-07-15 08:47:48 -0500

    .

--- src/api/authService.ts
+++ src/api/authService.ts
@@ -1,10 +1,25 @@
-import axios from "axios";
+/*import axios from "axios";
 
 const API = axios.create({
   baseURL: "http://65.38.96.125:3005"//"http://localhost:3005"
 });
 
-/* ----- Agrega automáticamente el token en cada petición ----- */
+/* ----- Agrega automáticamente el token en cada petición ----- /
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default API;*/
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3005"
+});
+
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
